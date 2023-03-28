c6bc65be50598f74b27c1c5af70da27aaf6b1818
rrsela <23569526+rrsela@users.noreply.github.com>
2023-03-28 23:15:57 +0300

    Outputs beautification

--- k8s/rke2/infra-terraform/outputs.tf
+++ k8s/rke2/infra-terraform/outputs.tf
@@ -2,28 +2,28 @@ output "vpc_id" {
   value = aws_vpc.rke2_vpc.id
 }
 
-output "nlb_id" {
-  value = local.nlb_id
-}
-
-output "nlb_private_dns" {
-  value = local.nlb_private_dns
-}
-
-output "nlb_public_dns" {
-  value = local.nlb_public_dns
-}
-
-output "public_subnet" {
+output "public_subnet_id" {
   value = aws_subnet.rke2_public.id
 }
 
-output "private_subnet" {
+output "private_subnet_id" {
   value = aws_subnet.rke2_private.id
 }
 
-output "k8s_sg" {
+output "security_groups_id" {
   value = aws_security_group.rke2_k8s.id
+}
+
+output "master_load_balancer_id" {
+  value = local.nlb_id
+}
+
+output "master_load_balancer_internal_dns" {
+  value = local.nlb_private_dns
+}
+
+output "master_load_balancer_external_dns" {
+  value = local.nlb_public_dns
 }
 
 output "bastion_ip" {
