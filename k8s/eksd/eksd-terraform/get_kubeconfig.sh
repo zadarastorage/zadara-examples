cd2000021c664c553dca16fa0e655ad59b8a968a
Roy Sela <roy.sela@zadarastorage.com>
2023-08-02 17:43:57 +0300

    Add retry to get_kubeconfig script

--- k8s/eksd/eksd-terraform/get_kubeconfig.sh
+++ k8s/eksd/eksd-terraform/get_kubeconfig.sh
@@ -19,14 +19,19 @@ bastion_keypair=$6
 master_user=$7
 master_keypair=$8
 
-# Copy the master keypair into the bastion
-scp -i $bastion_keypair -o StrictHostKeyChecking=no $master_keypair $bastion_user@$bastion_ip:~/master_keypair.pem
+# Copy the master keypair into the bastion and fix its permissions for further usage
+scp -i $bastion_keypair $master_keypair $bastion_user@$bastion_ip:~/master_keypair.pem
+ssh $bastion_user@$bastion_ip "chmod 400 ~/master_keypair.pem"
 
-# SSH into the bastion, fix the pem permissions and use it to fetch the key
-ssh $bastion_user@$bastion_ip "chmod 400 ~/master_keypair.pem ; scp -i ~/master_keypair.pem -o StrictHostKeyChecking=no ${master_user}@${master_hostname}:/etc/kubernetes/admin.conf ~/kubeconfig"
+# SSH into the bastion in order to fetch the kubeconfig from the master node (can take a while)
+while ssh $bastion_user@$bastion_ip "scp -i ~/master_keypair.pem -o StrictHostKeyChecking=no ${master_user}@${master_hostname}:/etc/kubernetes/admin.conf ~/kubeconfig" >& /dev/null; [[ $? -ne 0 ]];
+do
+    echo "Couldn't obtain the kubeconfig from the master node, sleeping before retrying"
+    sleep 5
+done
 
 # Fetch the kubeconfig from the bastion and cleanup the temp files on the bastion
-scp -i $bastion_keypair -o StrictHostKeyChecking=no $bastion_user@$bastion_ip:~/kubeconfig ./kubeconfig.temp
+scp -i $bastion_keypair $bastion_user@$bastion_ip:~/kubeconfig ./kubeconfig.temp
 ssh -i $bastion_keypair $bastion_user@$bastion_ip "rm -f ~/kubeconfig ~/master_keypair.pem"
 
 # Replace the original API-server endpoint from the internal IP to the pulic IP
