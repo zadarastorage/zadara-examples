f0d4aa3ae1997e02bd4ebfb084ca084c168e974c
Roy Sela <roy.sela@zadarastorage.com>
2023-10-12 16:10:30 +0300

    cosmetics

--- k8s/eksd/eksd-terraform/modules/asg/variables.tf
+++ k8s/eksd/eksd-terraform/modules/asg/variables.tf
@@ -6,7 +6,7 @@ variable "group_name" {
 variable "ebs_csi_volume_type" {
   type        = string
   default     = "gp2"
-  description = "VolumeType alias (defaulting to gp2 in order to align with zCompute default VolumeType)"
+  description = "VolumeType API alias (defaulting to gp2 in order to align with zCompute default VolumeType) for EBS CSI PVs"
   validation {
     condition     = contains(["io1", "io2", "gp2", "gp3", "sc1", "st1", "standard", "sbp1", "sbg1"], var.ebs_csi_volume_type)
     error_message = "Valid values for var: ebs_csi_volume_type are (io1, io2, gp2, gp3, sc1, st1, standard, sbp1, sbg1)."
@@ -16,7 +16,7 @@ variable "ebs_csi_volume_type" {
 variable "volume_type" {
   type        = string
   default     = null
-  description = "Defaulting to null in order to preserve the default type listed on zCompute"
+  description = "VolumeType API alias (defaulting to null in order to preserve the zCompute default type) for EC2 instances"
 }
 
 variable "volume_size" {
